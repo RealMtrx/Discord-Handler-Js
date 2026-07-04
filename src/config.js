232f51907c5a2490bd29c885366c4cea5a6e8bae
Mtrx-Gxy <mtrxturki@gmail.com>
2026-07-04 19:37:22 +0300

    Initial commit

---
+++ src/config.js
@@ -0,0 +1,16 @@
+import "dotenv/config";
+
+export default {
+    token: "#",
+    clientId: "#",
+    botName: "Discord Handler",
+    prefix: "$",
+    ownerIds: ["#", "#"],
+    MONGODB_URI: "#",
+    errorWebhook: "#",
+    slashCommandWebhook: "#",
+    prefixCommandWebhook: "#",
+    joinGuildWebhook: "#",
+    leaveGuildWebhook: "#",
+    readyWebhook: "#",
+};
